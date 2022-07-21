ea652caed88b06da4e0470c4f44895aa8663b9bb
Rudit Rahul <rufit.09@gmail.com>
2022-07-21 13:51:59 +0530

    Add to Cart APIs

--- Controllers/bookings.js
+++ Controllers/bookings.js
@@ -16,7 +16,7 @@ exports.addServicesToCart = async (req, res) => {
   try {
     const shopId = req.body.shopId;
     const userId = req.body.userId;
-    const service = req.body.service;
+    const service = req.body.services;
 
     //Search for cart whose booking is not done yet
     const booking = await Bookings.findOne({
@@ -43,6 +43,73 @@ exports.addServicesToCart = async (req, res) => {
       newBooking = await newBooking.save();
       return res.status(201).json({ status: true, data: newBooking });
     }
+  } catch (err) {
+    return res.status(500).json({ status: false, error: err.message });
+  }
+};
+
+exports.removeServicesFromCart = async (req, res) => {
+  try {
+    const serviceId = req.body.serviceId;
+    const bookingId = req.body.bookingId;
+
+    const updatedBooking = await Bookings.findByIdAndUpdate(
+      { _id: bookingId },
+      { $pull: { services: { subServiceId: serviceId } } },
+      { new: true }
+    );
+    return res.status(200).json({ status: true, data: updatedBooking });
+  } catch (err) {
+    return res.status(500).json({ status: false, error: err.message });
+  }
+};
+
+exports.getUserCart = async (req, res) => {
+  try {
+    const userId = req.params.userId;
+    const booking = await Bookings.findOne({
+      userId: userId,
+      isLive: true,
+    })
+      .populate({
+        path: "services",
+        populate: {
+          path: "subServiceId",
+        },
+      })
+      .populate({
+        path: "services",
+        populate: {
+          path: "subServiceOptions",
+        },
+      });
+    return res.status(200).json({ status: true, data: booking });
+  } catch (err) {
+    return res.status(500).json({ status: false, error: err.message });
+  }
+};
+
+exports.updateUserCart = async (req, res) => {
+  try {
+    const bookingId = req.body.bookingId;
+    const updatedCart = await Bookings.findByIdAndUpdate(
+      { _id: bookingId },
+      req.body,
+      { new: true }
+    )
+      .populate({
+        path: "services",
+        populate: {
+          path: "subServiceId",
+        },
+      })
+      .populate({
+        path: "services",
+        populate: {
+          path: "subServiceOptions",
+        },
+      });
+    return res.status(200).json({ status: true, data: updatedCart });
   } catch (err) {
     return res.status(500).json({ status: false, error: err.message });
   }
--- Routes/bookings.js
+++ Routes/bookings.js
@@ -4,5 +4,6 @@ const bookingController = require("../Controllers/bookings");
 
 router.post("/get-available-timeslots", bookingController.getAvailableTimeSlot);
 router.post("/add-services-to-cart", bookingController.addServicesToCart);
-
+router.get("/get-user-cart/:userId", bookingController.getUserCart);
+router.patch("/update-user-cart", bookingController.updateUserCart);
 module.exports = router;
