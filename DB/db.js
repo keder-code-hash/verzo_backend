029b1c330d07fe735b9a0aaff54b9695ae00cbf2
Keder Nath mallick <53966650+keder-code-hash@users.noreply.github.com>
2023-02-16 23:03:38 +0530

    adding bd

--- DB/db.js
+++ DB/db.js
@@ -1,8 +1,11 @@
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", true);
 mongoose
-  .connect("mongodb+srv://cluster0.i6will5.mongodb.net/verover", {
-    useNewUrlParser: true,
-  })
+  .connect(
+    "mongodb+srv://kedernath:Keder1234@verover.1otujbw.mongodb.net/?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+    }
+  )
   .then(() => console.log("Database connected"))
   .catch((err) => console.log("Error in database connection", err.message));
