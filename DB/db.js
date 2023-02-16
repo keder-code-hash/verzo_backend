0f85cdd0ed5f65fac521cab1fc0784135de49ed8
Keder Nath mallick <53966650+keder-code-hash@users.noreply.github.com>
2023-02-16 22:56:31 +0530

    added db string

--- DB/db.js
+++ DB/db.js
@@ -1,11 +1,8 @@
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", true);
 mongoose
-  .connect(
-    "mongodb+srv://saibal-roy:bwFzMmLTKEY4mgzs@freecluster.eiwhf.mongodb.net/verover",
-    {
-      useNewUrlParser: true,
-    }
-  )
+  .connect("mongodb+srv://cluster0.i6will5.mongodb.net/verover", {
+    useNewUrlParser: true,
+  })
   .then(() => console.log("Database connected"))
   .catch((err) => console.log("Error in database connection", err.message));
